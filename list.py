599e77627764f3976f10ba863da05a31f71efdad
prajakta1611 <gawasprachi95@gmail.com>
2023-07-05 21:46:32 +0530

    update

---
+++ list.py
@@ -0,0 +1,64 @@
+l=[2,10,26,5,8,3,"4","hello",5.6,"python"]
+l2=[2,1]
+extend_list=[3,22]
+
+try:
+    class List:
+        """This function contain all the list functions."""
+        def __init__(self,list):
+            self.list=list
+        def Listlen(self):
+            """function to find length."""
+            print("len of list :",len(self.list))
+        def ListTyp(self):
+            """Function to check type."""
+            print("Type of list :",type(self.list))
+        def ListOfIntVal(self,list2):
+            """append function."""
+            self.list2=list2
+            for i in self.list:
+                if type(i) == int or type(i) == float:
+                    self.list2.append(i)
+            print("new list is :",self.list2)
+        def ListExtend(self):
+            """Extendfunction"""
+            self.list.extend(self.list2)
+            print("extended list :",self.list) 
+        def ListSort(self):
+            """sort function"""
+            self.list2.sort()
+            print("sorted list is :",self.list2)
+            self.list2.sort(reverse=True)
+            print("sorted list is :",self.list2)
+        def listMinMax(self):
+            """Min max function"""
+            print("max value of new list :",max(self.list2))   
+            print("min value of new list :",min(self.list2)) 
+        def listInsertVal(self,index:int,value:int):
+            """insert function"""
+            self.list2.insert(index,value)
+            print("inserted value :",self.list2)
+        def ListIndex(self,index:int):
+            """Indexfunction"""
+            print("index of ",index," is :",self.list2.index(index))
+        def ListRemove(self,value:int):
+            """remove function"""
+            print("delete ",value,"value :",self.list2.remove(value))
+            print("after deliting ",value,"value :",self.list2)
+        def ListPop(self):
+            """pop function"""
+            print("remov last value :",self.list2.pop())
+            print("after removing last value :",self.list2)
+    ob=List(l)
+    ob.Listlen()
+    ob.ListTyp()
+    ob.ListOfIntVal(l2)
+    ob.ListExtend()
+    ob.ListSort()
+    ob.listMinMax()
+    ob.listInsertVal(9,99)
+    ob.ListIndex(99)
+    ob.ListRemove(2)
+    ob.ListPop()
+except Exception as e:
+    print(e)
---
+++ tuple.py
@@ -0,0 +1,36 @@
+t=(2,10,26,5,8,3,8,8,8,"4","hello",5.6,"python")
+t1=(1)
+t2=(1,2,6,4,32,34,1,2,3,44,55)
+
+class tuple:
+    """"THIS CLASS CONTAIN EVERYTHING ABOUT TUPLE"""
+    def __init__(self,tuple):
+        self.tuple=tuple
+    def TuplePrintT(self):
+        """FUNCTION TO PRINT THE GIVEN TUPLE"""
+        print(self.tuple)
+    def TupleType(self,tup):
+        """"FUNCTION TO FIND THE TYPE """
+        print("type of tuple :",type(tup))
+        print("type of tuple :",type(self.tuple))
+    def Tuplelen(self):
+        """"FUNCTION TO PRINT THE LEN """
+        print("len of the given tuple :",len(self.tuple))
+    def TupleValue(self):
+        """"FUNCTION TO EXTRACT THE VALUE """
+        print(self.tuple[0:5])
+        print(self.tuple[6])
+    def TupleCount(self,a:int):
+        """"FUNCTION TO COUNT THE OCCURANCE OF THE VALUE """
+        print(self.tuple.count(a))
+    def TupleMaxMin(self,tuplem):
+        """"FUNCTION TO FIND THE MAX MIN VALUE """
+        print(max(tuplem))
+        print(min(tuplem))
+ob=tuple(t)
+ob.TuplePrintT()
+ob.TupleType(t1)
+ob.Tuplelen()
+ob.TupleValue()
+ob.TupleCount(8)
+ob.TupleMaxMin(t2)

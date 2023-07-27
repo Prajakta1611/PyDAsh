47e8bc8497237498de4c3c13e8b8be5f4eb4629d
prajakta1611 <gawasprachi95@gmail.com>
2023-07-27 18:11:03 +0530

    update

---
+++ pattern/patter.py
@@ -0,0 +1,12 @@
+#program to print the following pattern
+""" *
+    **
+    ***
+    ****
+    *****
+"""
+
+for i in range(6):
+    for j in range(1,i+1):
+        print("*",end ='')
+    print('')
